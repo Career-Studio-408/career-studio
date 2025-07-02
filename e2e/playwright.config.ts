eeb110c5aa6bee7d88656830f68992530d04d3cc
mrtaz77 <mrtazwar23@gmail.com>
2025-07-02 13:42:06 +0600

    feat: update Playwright configuration and add test scripts in package.json

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -4,9 +4,12 @@ export default defineConfig({
   testDir: './tests',
   timeout: 30 * 1000,
   retries: 0,
+  reporter: [['list'], ['html']],
   use: {
     baseURL: 'http://localhost:8080',
     headless: true,
     screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
+    video: 'retain-on-failure',
   },
 })
--- e2e/util/env.ts
+++ e2e/util/env.ts
@@ -3,4 +3,4 @@ dotenv.config()
 
 export const USER_1_EMAIL = process.env.USER_1_EMAIL!
 export const USER_1_PASSWORD = process.env.USER_1_PASSWORD!
-export const BASE_URL = process.env.BASE_URL!
+export const BASE_URL = process.env.BASE_URL || 'http://localhost:8080'
